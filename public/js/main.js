586fe8cc66d2d21af118af27258a5c5633b99190
Alexandre <alesirius@gmail.com>
2019-05-26 15:36:47 -0300

    deletar linhas dinamicamente

--- public/js/main.js
+++ public/js/main.js
@@ -71,21 +71,33 @@ function inserePlacar(){
     let corpoTabela = $(".placar").find("tbody");
     let numPalavras = $("#contador-palavras").text();
     let usuario = "Alexandre";
-    let botaoRemover = "<a href='#'><i class='material-icons'>delete_forever</i></a>";
-
-    let linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                    "<td>"+ botaoRemover + "</td>"+
-                "</tr>";
+    let linha = novaLinha(usuario, numPalavras);   
+    linha.find(".botao-remover").click(removeLinha)      
     corpoTabela.append(linha);            
 }
 
-$(".botao-remover").click((event)=>{
-    event.preventDefault();
-    $(this).parent().parent().remove();
-});
+function novaLinha(usuario, numPalavras){
+    let linha = $("<tr>");
+    let colunaUsuario = $("<td>").text(usuario);
+    let colunaPalavras = $("<td>").text(numPalavras);
+    let colunaRemover = $("<td>");
+    let link = $("<a>").addClass("botao-remover").attr("href", "#");
+    let icone = $("<i>").addClass("material-icons").text("delete_forever");
 
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+function removeLinha(){
+    $(".botao-remover").click(function(event){
+        event.preventDefault();
+        $(this).parent().parent().remove();
+    });
+}
 
 function reiniciaJogo(){
     
